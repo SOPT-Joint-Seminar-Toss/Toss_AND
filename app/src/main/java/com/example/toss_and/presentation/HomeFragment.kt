b089cb11507e7c1fed3785026039d5814bccd94e
Skylar <olfaminion0116@naver.com>
2023-05-20 06:00:10 +0900

    [layout] 빈 HomeFragment 생성

---
+++ app/src/main/java/com/example/toss_and/presentation/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.example.toss_and.presentation
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.toss_and.R
+import com.example.toss_and.databinding.FragmentHomeBinding
+import com.example.toss_and.util.base.BindingFragment
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
