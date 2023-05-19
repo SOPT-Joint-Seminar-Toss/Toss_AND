72197ff78d376e184153e9810f9d798421b9c32d
Skylar <olfaminion0116@naver.com>
2023-05-20 08:46:56 +0900

    [layout] 홈 화면 일부 구성 (미완)

--- app/src/main/java/com/example/toss_and/presentation/HomeActivity.kt
+++ app/src/main/java/com/example/toss_and/presentation/HomeActivity.kt
@@ -8,5 +8,9 @@ import com.example.toss_and.util.base.BindingActivity
 class HomeActivity: BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.cl_fragment_container, HomeFragment())
+            .commit()
     }
 }
