4714533b6e5e9b5b0bc51d289dad275bdfdf3ac8
Skylar <olfaminion0116@naver.com>
2023-05-24 12:34:44 +0900

    [layout] viewpager2 -> supportFragmentManager 변경

--- app/src/main/java/com/example/toss_and/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/toss_and/presentation/main/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.toss_and.presentation.main
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
-import androidx.viewpager2.widget.ViewPager2
+import androidx.fragment.app.Fragment
 import com.example.toss_and.R
 import com.example.toss_and.databinding.ActivityMainBinding
 import com.example.toss_and.presentation.MainViewModel
+import com.example.toss_and.presentation.TempPayFragment
+import com.example.toss_and.presentation.main.all.AllFragment
+import com.example.toss_and.presentation.main.benefit.BenefitFragment
+import com.example.toss_and.presentation.main.home.HomeFragment
+import com.example.toss_and.presentation.main.stock.StockFragment
 import com.example.toss_and.util.base.BindingActivity
 
 
@@ -15,8 +20,8 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setViewPager()
         setNavigation()
+        binding.bottomNav.selectedItemId = R.id.nav_home
 
         /* TODO: activity에서 쓴다면 이런 식으로 쓰면 되겠찌 >< */
         mainVm.getAssets()
@@ -25,54 +30,42 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         }
     }
 
-    private fun setViewPager() {
-        binding.viewPager.adapter = MainPagerAdapter(this)
-        // 슬라이드해서 페이지가 변경되면 bottom nav의 탭도 그 페이지로 활성화
-        binding.viewPager.registerOnPageChangeCallback(
-            object : ViewPager2.OnPageChangeCallback() {
-                override fun onPageSelected(position: Int) {
-                    super.onPageSelected(position)
-                    binding.bottomNav.menu.getItem(position).isChecked = true
-                }
-            }
-        )
-    }
-
     private fun setNavigation() {
         binding.bottomNav.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.nav_home -> {
-                    binding.viewPager.currentItem = 0
-                    //setStatusBarColor(R.color.grey_100)
+                    replaceFragment(HomeFragment())
                     return@setOnItemSelectedListener true
                 }
 
                 R.id.nav_benefit -> {
-                    binding.viewPager.currentItem = 1
-                    //setStatusBarColor(R.color.white)
+                    replaceFragment(BenefitFragment())
                     return@setOnItemSelectedListener true
                 }
 
                 R.id.nav_tosspay -> {
-                    binding.viewPager.currentItem = 2
-                    //setStatusBarColor(R.color.white)
+                    replaceFragment(TempPayFragment())
                     return@setOnItemSelectedListener true
                 }
 
                 R.id.nav_stock -> {
-                    binding.viewPager.currentItem = 3
-                    //setStatusBarColor(R.color.white)
+                    replaceFragment(StockFragment())
                     return@setOnItemSelectedListener true
                 }
 
                 R.id.nav_all -> {
-                    binding.viewPager.currentItem = 4
-                    //setStatusBarColor(R.color.white)
+                    replaceFragment(AllFragment())
                     return@setOnItemSelectedListener true
                 }
 
                 else -> return@setOnItemSelectedListener false
             }
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
