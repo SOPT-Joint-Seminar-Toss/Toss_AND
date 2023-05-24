1710ebbe0f183a11d6423472d11603864cb356c5
Skylar <olfaminion0116@naver.com>
2023-05-24 11:35:22 +0900

    [layout] 일부 에셋 추가, 간격 일부 조정

--- app/src/main/java/com/example/toss_and/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/toss_and/presentation/main/MainActivity.kt
@@ -6,9 +6,10 @@ import androidx.activity.viewModels
 import androidx.viewpager2.widget.ViewPager2
 import com.example.toss_and.R
 import com.example.toss_and.databinding.ActivityMainBinding
+import com.example.toss_and.presentation.MainViewModel
 import com.example.toss_and.util.base.BindingActivity
 
-class MainActivity: BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val mainVm by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +28,7 @@ class MainActivity: BindingActivity<ActivityMainBinding>(R.layout.activity_main)
         binding.viewPager.adapter = MainPagerAdapter(this)
         // 슬라이드해서 페이지가 변경되면 bottom nav의 탭도 그 페이지로 활성화
         binding.viewPager.registerOnPageChangeCallback(
-            object: ViewPager2.OnPageChangeCallback() {
+            object : ViewPager2.OnPageChangeCallback() {
                 override fun onPageSelected(position: Int) {
                     super.onPageSelected(position)
                     binding.bottomNav.menu.getItem(position).isChecked = true
@@ -38,27 +39,32 @@ class MainActivity: BindingActivity<ActivityMainBinding>(R.layout.activity_main)
 
     private fun setNavigation() {
         binding.bottomNav.setOnItemSelectedListener {
-            when(it.itemId) {
+            when (it.itemId) {
                 R.id.nav_home -> {
                     binding.viewPager.currentItem = 0
                     return@setOnItemSelectedListener true
                 }
+
                 R.id.nav_benefit -> {
                     binding.viewPager.currentItem = 1
                     return@setOnItemSelectedListener true
                 }
+
                 R.id.nav_tosspay -> {
                     binding.viewPager.currentItem = 2
                     return@setOnItemSelectedListener true
                 }
+
                 R.id.nav_stock -> {
                     binding.viewPager.currentItem = 3
                     return@setOnItemSelectedListener true
                 }
+
                 R.id.nav_all -> {
                     binding.viewPager.currentItem = 4
                     return@setOnItemSelectedListener true
                 }
+
                 else -> return@setOnItemSelectedListener false
             }
         }
